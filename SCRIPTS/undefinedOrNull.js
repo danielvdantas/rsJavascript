f4e2158f68a5efa5d08ed8784de20c6c5a341e0c
danielvdantas <danielvdantas@hotmail.com>
2022-07-04 17:03:12 -0300

    aula 5 - undefined vs Null

---
+++ SCRIPTS/undefinedOrNull.js
@@ -0,0 +1,12 @@
+/*
+    * undefined // indefinido
+
+    * Null
+        - Nulo
+        - objeto que não tem nada dentro
+        - diferente de indefinido
+*/
+
+console.log(undefined)
+console.log(null)
+console.log(null === undefined) // retorna false
