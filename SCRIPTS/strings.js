7706b353f9c493df90aa9f9c7bb25937ae41e705
danielvdantas <danielvdantas@hotmail.com>
2022-07-04 16:39:09 -0300

    Aula de String

---
+++ SCRIPTS/strings.js
@@ -0,0 +1,13 @@
+/*
+    String
+
+    * Cadeia de caracteres
+
+    "" // Aspas Duplas
+    '' // Aspas Simples
+    `` // Template Literals ou Template strings
+*/
+
+console.log("Daniel") // Posso usar no texto aspas simples, mas não aspas duplas, pois dá conflito
+console.log('Daniel') // Posso usar no texto aspas duplas, mas não aspas simples, pois dá conflito
+console.log(`Daniel ${1 + 1}`) // Além de poder usar as opções de aspas anteriores nos textos, permite quebra de linha e expressões de linguagem (interpolação).
