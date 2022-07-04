12a28772073a2ae5efe94ca9bdeafb249afe867b
danielvdantas <danielvdantas@hotmail.com>
2022-07-04 16:55:13 -0300

    Aula 4 - Boolean

---
+++ SCRIPTS/boolean.js
@@ -0,0 +1,11 @@
+/*
+    Boolean
+
+    * Somente 2 valores (true || false)
+    * necessário para fazer lógica de programação
+    true // verdadeiro
+    false // falso
+*/
+
+console.log(true)
+console.log(false)
