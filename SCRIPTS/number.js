4cf8457eafffb0d024cf867b3c118af8ca44885a
danielvdantas <danielvdantas@hotmail.com>
2022-07-04 16:49:19 -0300

    Exercicio de números

---
+++ SCRIPTS/number.js
@@ -0,0 +1,15 @@
+/*
+    Number
+
+    * numeros
+
+    33 // inteiros
+    12.5 // reais - float
+    NaN // Noa a number
+    Infinity // infinito
+*/
+
+console.log(33) // Inteiro
+console.log(12.5) // float
+console.log(12.5 / "asdf") // NaN
+console.log(12.5 == Infinity) // Verifica se é verdadeiro ou falso. Quase não é usado
