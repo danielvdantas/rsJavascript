7865b38a512ad1a2cdb1914ba2a345c57c3d765d
danielvdantas <danielvdantas@hotmail.com>
2022-07-04 17:11:32 -0300

    Aula 6 - Object

---
+++ SCRIPTS/Object.js
@@ -0,0 +1,20 @@
+/*
+    Object
+        * objeto
+        * propriedades / Atributos
+        * funcionalidades/ métodos
+        
+        {propriedade: "valor"} // sintaxe
+*/
+
+// imprime um objeto vazio
+console.log({})
+
+// imprime um objeto com propriedade e até função dentro.
+console.log({
+    nome: "Daniel",
+    idade: 46,
+    andar: function() {
+        console.log('andar')
+    }
+})
