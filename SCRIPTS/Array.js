83f7bc6c6347e7043885177aa5b90634b51e1928
danielvdantas <danielvdantas@hotmail.com>
2022-07-05 09:28:37 -0300

    Aula 7 - Array

---
+++ SCRIPTS/Array.js
@@ -0,0 +1,10 @@
+/*
+    Array (Vetores)
+        * Uma lista
+        * Agrupamento de dados
+        
+    ["Daniel", 46]
+*/
+
+// Cada dado separado pr uma vírgula. Aceita quebra de linha apos a vírgula.
+console.log(['Daniel', 46])
