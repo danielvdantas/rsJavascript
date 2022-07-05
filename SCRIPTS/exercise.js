11a388afaeab001bd12156b9ada97b2b10b17df3
danielvdantas <danielvdantas@hotmail.com>
2022-07-05 12:18:34 -0300

    Fim dos exercícios

--- SCRIPTS/exercise.js
+++ SCRIPTS/exercise.js
@@ -1,8 +1,8 @@
 // 1. Declare uma variável de nome weight
-let weight;
+// let weight;
 
 // 2. Que tipo de dado é a variável acima?
-//console.log(typeof weight)
+// console.log(typeof weight)
 
 /*
    3. Declare uma variável e atribua valores para cada um dos dados:
@@ -29,25 +29,25 @@ isSubscribed = true*/
     Atenção, substitua <name> <age> e <weight> pelos valores de cada propriedade do objeto.
 */
 
-let student = {
+/*let student = {
     name: "Daniel", 
     age: 33, 
     weight: 88.1
-};
+};*/
 
 //console.log(typeof student)
 //console.log(`${student.name} de idade ${student.age}, pesa ${student.weight} kg.`);
 
 /*
-    5. Declare uma variável tipo Array de nome students e atribua a ela nehum valor, ou seja, um Array vazio.
+    5. Declare uma variável tipo Array de nome students e atribua a ela nenhum valor, ou seja, um Array vazio.
 */
-    let students = [];
+//let students = [];
 /*
     6. Reatribua um valor a variável acima, colocando dentro dela o objeto student da questçao 4. (nâo copiar e colar o objeto, mas usar o objeto criado e inserir ele no Array.)
 */
-students = [
+/*students = [
     student
-]
+]*/
 //console.log(students)
 
 /*
@@ -58,6 +58,7 @@ students = [
 /*
     7. Crie um novo student e coloque ele na posição 1 do Array students:
 */
+/*
 const john = {
     name: "John",
     age: 23,
@@ -69,16 +70,15 @@ students = [
     student,
     john
 ]
-//console.log(students)
-    
-    /*
-        9. Sem rodar o código, qual a resposta do código abaixo e porque? Após sua resposta, rode o código e veja se você acertou.
+console.log(students)*/
 
-        console. log(a)
-        var a = 1;
 
-        Resposta: A resposta será 'undefined', pois o valor da variável a foi atribuída após a função, porém a variável foi declarada atraves do hoisting.
-    */
+//9. Sem rodar o código, qual a resposta do código abaixo e porque? Após sua resposta, rode o código e veja se você acertou.
 
-        console.log(a);
-        var a = 1;
+console.log(a)
+var a = 1;
+
+//Resposta: A resposta será 'undefined', pois o valor da variável a foi atribuída após a função, porém a variável foi declarada atraves do hoisting.
+
+console.log(a);
+var a = 1;
